bd8513c9a2f56ddd623d882b5c82f7e50d61707f
Ahmed El Gendy <elgendyahmed753@gmail.com>
2023-12-14 17:26:38 +0200

    test'

--- app/src/main/java/com/example/sagendy/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/sagendy/ui/home/HomeViewModel.java
@@ -1,13 +1,16 @@
 package com.example.sagendy.ui.home;
 
+import android.widget.Button;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.sagendy.R;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
-
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
