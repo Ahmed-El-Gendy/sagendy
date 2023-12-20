d881ddefe5e53ff78bb89707826ba6ddcc77269d
Ahmed El Gendy <elgendyahmed753@gmail.com>
2023-12-20 14:36:44 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 buildscript {
+    val agp_version by extra("8.1.4")
+    val agp_version1 by extra("8.1.4")
+    val agp_version2 by extra("8.2")
+    val agp_version3 by extra("8.2.0")
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
     }
