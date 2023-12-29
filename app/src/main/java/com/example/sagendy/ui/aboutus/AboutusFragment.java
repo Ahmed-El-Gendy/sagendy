73615136c069867623104871fbc779d90d5d02bf
Ahmed El Gendy <elgendyahmed753@gmail.com>
2023-12-29 17:47:31 +0200

    lang done

---
+++ app/src/main/java/com/example/sagendy/ui/aboutus/AboutusFragment.java
@@ -0,0 +1,33 @@
+package com.example.sagendy.ui.aboutus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sagendy.R;
+import com.example.sagendy.databinding.FragmentSlideshowBinding;
+import com.example.sagendy.ui.slideshow.SlideshowViewModel;
+
+public class AboutusFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_aboutus, container, false);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
